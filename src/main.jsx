9171fbb41bd1123a40ddb16d586853cc86bc7e0c
Duc <kieungocanhduc@gmail.com>
2024-01-07 14:33:38 +0700

    update redux config

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import { ScheduleContextProvider } from "./store/schedule-context.jsx"
 import "../i18n.js"
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns"
+import { Provider } from "react-redux"
+import store from "./store/index.jsx"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ScheduleContextProvider>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </LocalizationProvider>
     </ScheduleContextProvider>
   </React.StrictMode>
